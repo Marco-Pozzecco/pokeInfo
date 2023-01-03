6882dbc16d855cfae464933e5cb4b3e1a3f02cb5
Marco-Pozzecco <pozzecco.marco@gmail.com>
2023-01-03 15:27:45 +0100

    feat: search bar functionality

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,3 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function SearchBar () {}
+export default function SearchBar() {
+  const [searchParam, setSearchParam] = useState<string>("");
+
+  function handleOnChange(e: React.FormEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value;
+    setSearchParam(value);
+  }
+
+  return (
+    <div className="poke-search">
+        <input
+          onChange={handleOnChange}
+          value={searchParam}
+          placeholder="Search pokemon.."
+          type="text"
+          className="poke-search-input"
+        />
+    </div>
+  );
+}
