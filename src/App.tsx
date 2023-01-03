daf6c4e03801721a31e1b303af234abcaa696638
Marco-Pozzecco <pozzecco.marco@gmail.com>
2023-01-03 16:19:31 +0100

    basic API setup in APP component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import SearchBar from './components/search-bar/SearchBar';
+import { PokemonClient, NamedAPIResource } from "pokenode-ts";
+import React, { useEffect, useState } from "react";
+import SearchBar from "./components/search-bar/SearchBar";
 
 function App() {
-  
+  const [pokedex, setPokedex] = useState<NamedAPIResource[] | null>(null);
+  const [pageIndex, setPageIndex] = useState<number>(1);
+  const [resultsLimit, setResultsLimit] = useState<number>(10);
+
+  const api = new PokemonClient();
+
+  async function handlePokemonSpeciesReq() {
+    await api.listPokemonSpecies(resultsLimit * (pageIndex - 1), resultsLimit)
+      .then((res) => {
+        setPokedex(res.results);
+      });
+  }
+
+  useEffect(() => {
+    handlePokemonSpeciesReq();
+  }, []);
 
   return (
     <div className="pokedex-app">
-      <div></div>
       <SearchBar />
+      <div></div>
+      {pokedex?.map((pokemon) => {
+        return <div>{pokemon.name}</div>;
+      })}
+      <div className="pokedex-pages"></div>
     </div>
   );
 }
